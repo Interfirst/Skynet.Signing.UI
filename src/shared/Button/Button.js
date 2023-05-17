fb0ee50b621185df3b778ce07a687949b5a43a77
Roman <roman.remnyov@interfirst.com>
2023-05-17 15:36:36 +0300

    feat(signing): update text button's font color

--- src/shared/Button/Button.js
+++ src/shared/Button/Button.js
@@ -7,7 +7,7 @@ import BrandSettingsContext from '../../contexts/brandSettings';
 const ButtonStyled = styled.button({
   color: ({ variant, $brandSettings }) => {
     if (variant === 'text') {
-      return get($brandSettings, 'colors.formControls.infoButton.textColor');
+      return get($brandSettings, 'colors.typography.label.labelTextColor');
     }
 
     return get($brandSettings, 'colors.formControls.containedButton.textColor');
